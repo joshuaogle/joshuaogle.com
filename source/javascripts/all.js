25748551b8d8324e990ed3a4012c2c8dd838cf65
Joshua Ogle <joshua@joshuaogle.com>
2016-08-14 19:03:06 -0600

    Add Proteus logo

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,10 +1,23 @@
+function isOnScreen($element) {
+  var elementPosition = $element.offset();
+  var elementTop = elementPosition.top;
+  var scrollTop = window.pageYOffset;
+  var screenHeight = $(window).height();
+  var screenBottom = scrollTop + screenHeight;
+  var offset = 50;
+
+  return screenBottom > (elementTop + offset);
+}
+
 $(function() {
-  $(".animate").each(function(i, el) {
-    var $el = $(el);
-    console.log($el.is(":in-viewport(100)"));
-    if ($el.isInViewport({tolerance: 3000})) {
-      console.log("go");
-      $el.addClass("animated");
-    }
-  });
+  window.onscroll = function () {
+    $(".animate").each(function(i) {
+      var $el = $(this);
+      if (!$el.hasClass("animated")) {
+        if (isOnScreen($el)) {
+          $el.addClass("animated");
+        }
+      }
+    });
+  };
 });
