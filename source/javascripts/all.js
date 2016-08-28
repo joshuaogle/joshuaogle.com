07373b8f2b6e6ba6642f95f93268a1755c4db667
Joshua Ogle <joshuaogle@joshuaogle.local>
2016-08-27 19:47:41 -0600

    Update button styles, fix links in summaries

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,28 +13,57 @@ function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
 		var context = this, args = arguments;
+		var callNow = immediate && !timeout;
+
 		var later = function() {
 			timeout = null;
 			if (!immediate) func.apply(context, args);
 		};
-		var callNow = immediate && !timeout;
+
 		clearTimeout(timeout);
 		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
+		if (callNow) {
+      func.apply(context, args)
+    };
 	};
 };
 
 var markAnimated = debounce(function() {
   $(".animate").each(function(i) {
-    var $el = $(this);
-    if (!$el.hasClass("animated")) {
-      if (isOnScreen($el)) {
-        $el.addClass("animated");
+    var $element = $(this);
+
+    if (!$element.hasClass("animated")) {
+      if (isOnScreen($element)) {
+        $element.addClass("animated");
       }
     }
   });
 });
 
+var fadeIntro = function() {
+  var scrollTop = window.pageYOffset;
+  $(".intro").each(function(i) {
+    var $intro = $(this);
+    var introHeight = $intro.height();
+    var percent = scrollTop / introHeight;
+    var slideUp = "translateY(-" + ((introHeight / 5) * percent) + "px)"
+    var zoomOut = "scale(" + (1 - (percent / 10)) + ")"
+    var blur = "blur(" + (percent * 5) + "px)"
+
+    $intro.find(".intro-content").css({
+      opacity: 1 - percent,
+      transform: slideUp + " " + zoomOut,
+      webkitFilter: blur
+    });
+  });
+};
+
 $(function() {
-  window.addEventListener("scroll", markAnimated);
+  if ($(".animate").length > 0) {
+    window.addEventListener("scroll", markAnimated);
+  }
+
+  if ($(".intro-content").length > 0) {
+    window.addEventListener("scroll", fadeIntro);
+  }
 });
