2eea4ae7ad64d631ec890720a77b257a97b79138
Joshua Ogle <joshua@joshuaogle.com>
2017-03-19 23:08:35 -0600

    Add resume

--- source/pages/About/index.jsx
+++ source/pages/About/index.jsx
@@ -19,8 +19,11 @@ class About extends React.Component {
           <section>
             <p>
               For over ten years, I've been helping companies of all sizes to establish their brand and <mark>improve their connection to their users</mark>.
-              I live in Denver, where I work at <a href="http://citrusbyte.com">Citrusbyte</a>, helping companies of all sizes realize their ideal product.
-              I previously worked at <a href="http://thoughtbot.com">thoughtbot</a>, and before that I was co-founder and Creative Director of a consultancy in OKC called Brightbit.
+              I live in Denver, where I work at <a href="http://citrusbyte.com" target="_new">Citrusbyte</a>, helping companies of all sizes realize their ideal product.
+              I previously worked at <a href="http://thoughtbot.com" target="_new">thoughtbot</a>, and before that I was co-founder and Creative Director of a consultancy in OKC called Brightbit.
+            </p>
+            <p>
+              <a href="/files/joshuaogle-resume.pdf" target="_new">You can read more about my work history on my resume</a>
             </p>
             <p>
               I have a passion for visual communication, problem-solving, usability, and storytelling.
