e545728c2ce0b5c98bd4e3bba6431416d2b5eba9
Joshua Ogle <joshua@joshuaogle.com>
2017-05-26 17:44:19 -0600

    Production build

--- source/pages/Work/Gallery.jsx
+++ source/pages/Work/Gallery.jsx
@@ -36,20 +36,20 @@ class Gallery extends React.Component {
         <section>
           {this.renderImage("armada-users", "case-studies/armada-users.png")}
           {this.renderImage("sharespost-chart", "case-studies/sharespost-chart.png")}
-          {this.renderImage("summit-map", "case-studies/summit-map.png")}
-          {this.renderImage("summit-popover", "case-studies/summit-popover.png", 1.5)}
-          {this.renderImage("armada-home", "case-studies/armada-home.png")}
+          {this.renderImage("summit-map", "case-studies/summit-map.png", 1.25)}
+          {this.renderImage("summit-popover", "case-studies/summit-popover.png", 0.75)}
+          {this.renderImage("armada-home", "case-studies/armada-home.png", 1)}
           {this.renderImage("armada-user", "case-studies/armada-user.png")}
-          {this.renderImage("nom-practice", "gallery-nom-practice.png")}
+          {this.renderImage("nom-practice", "gallery-nom-practice.png", 0.5)}
           {this.renderImage("cmr-wires", "gallery-cmr-wires.jpg")}
-          {this.renderImage("expedia-fb", "gallery-expedia-fb.jpg", 0.25)}
-          {this.renderImage("smartpanel", "gallery-smartpanel.jpg", 0.75)}
-          {this.renderSvg("brand-armada", 1.25)}
-          {this.renderSvg("brand-wifiguru", 1.5)}
-          {this.renderSvg("brand-brightbit")}
-          {this.renderSvg("brand-proteus", 0.75)}
+          {this.renderImage("expedia-fb", "gallery-expedia-fb.jpg")}
+          {this.renderImage("smartpanel", "gallery-smartpanel.jpg")}
+          {this.renderSvg("brand-armada", 0.5)}
+          {this.renderSvg("brand-wifiguru", 1)}
+          {this.renderSvg("brand-brightbit", 0.5)}
+          {this.renderSvg("brand-proteus")}
           {this.renderSvg("brand-gemini")}
-          {this.renderSvg("brand-skyhelm", 1)}
+          {this.renderSvg("brand-skyhelm")}
         </section>
       </div>
     );
