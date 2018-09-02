ed73b3fab95d45e5ba2053f9c172a16581ecf5ed
Joshua Ogle <joshua@joshuaogle.com>
2018-09-02 17:21:30 -0600

    Fix work page

--- source/sections/Clients/index.jsx
+++ source/sections/Clients/index.jsx
@@ -9,19 +9,6 @@ class Clients extends React.Component {
       <section className="clients">
         <div className="section-title">Clients</div>
         <h2>I've worked with companies of all sizes</h2>
-        <p>
-          I've been lucky enough to work with some of the biggest companies in the world,
-          but most of my career has been spent working with startups and small businesses
-          making their start in the world and bringing their ideas to life.
-          The beginning of a venture is a truly exciting time, where every decision is important
-          and everyone on the project makes a huge impact.
-          I believe in starting small, iterating quickly, and listening to what your users are telling you.
-          I bring that same approach to projects no matter the size to make every project succeed.
-        </p>
-
-        <p>
-          Here are a few clients you might have heard of:
-        </p>
         <div className="clients-logos">
           <div className="client-logo" data-name="Expedia">
             <InlineSVG src="client-expedia.svg" />
@@ -39,6 +26,15 @@ class Clients extends React.Component {
             <InlineSVG src="client-sharespost.svg" />
           </div>
         </div>
+        <p>
+          I've been lucky enough to work with some of the biggest companies in the world,
+          but most of my career has been spent working with startups and small businesses
+          making their start in the world and bringing their ideas to life.
+          The beginning of a venture is a truly exciting time, where every decision is important
+          and everyone on the project makes a huge impact.
+          I believe in starting small, iterating quickly, and listening to what your users are telling you.
+          I bring that same approach to projects no matter the size to make every project succeed.
+        </p>
       </section>
     );
   }
