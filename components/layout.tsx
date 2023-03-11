7c4b0eb91647fad76af5dac41515446e48ad857c
Joshua Ogle <joshua@joshuaogle.com>
2023-03-11 16:21:45 -0700

    Styles in components

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
-import Header from './header'
-import Footer from './footer'
-import Meta from './meta'
+import Header from './header';
+import Footer from './footer';
+import Meta from './meta';
 
 type Props = {
   preview?: boolean
@@ -9,14 +9,22 @@ type Props = {
 
 const Layout = ({ preview, children }: Props) => {
   return (
-    <>
+    <div className="app">
       <Meta />
-      <div className="min-h-screen">
-        <Header />
-        <main>{children}</main>
-      </div>
+      <Header />
+      <main className="page">
+        {children}
+      </main>
       <Footer />
-    </>
+
+      <style jsx>{`
+        .page {
+          margin: 0 auto;
+          max-width: var(--page-max-width);
+          overflow: hidden;
+        }
+        `}</style>
+    </div>
   )
 }
 
