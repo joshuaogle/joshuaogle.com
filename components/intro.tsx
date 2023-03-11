ecaf5c824bc5d583c710425052dacab33e83d9a0
Joshua Ogle <joshua@joshuaogle.com>
2023-03-11 16:00:28 -0700

    Add Sass and figure out CSS variables

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Intro extends React.Component {
   render() {
     return (
-      <div className={`intro ${this.props.className}`} style={this.props.styles}>
+      <div className="intro container">
         <div className="intro-content">
           {this.props.children}
         </div>
