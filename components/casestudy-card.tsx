2c96fb4194424741635e4957d1795fd08f326694
Joshua Ogle <joshua@joshuaogle.com>
2023-06-08 11:23:58 -0600

    Make case study cards smaller

--- components/casestudy-card.tsx
+++ components/casestudy-card.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/_casestudy-card.module.css'
 
 type Props = {
   summary: any,
-  href: any,
+  href: string,
 }
 
 const CasestudyCard = ({summary, href}: Props) => {
@@ -21,12 +21,14 @@ const CasestudyCard = ({summary, href}: Props) => {
         <div className={styles.icon}>
           <img src={summary.theme.icon} />
         </div>
-        <h4>
-          {summary.title}
-        </h4>
-        <small className={styles.meta}>
-          {summary.meta.role}
-        </small>
+        <div className={styles.copy}>
+          <h4>
+            {summary.title}
+          </h4>
+          <small className={styles.meta}>
+            {summary.meta.role}
+          </small>
+        </div>
       </article>
     </Link>
   )
