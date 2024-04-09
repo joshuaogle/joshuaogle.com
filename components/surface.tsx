892a27446cfe501e5b5f33acddf0040afa0f451f
Joshua Ogle <joshua@joshuaogle.com>
2024-04-09 17:40:32 -0600

    Fix build error

--- components/surface.tsx
+++ components/surface.tsx
@@ -1,7 +1,8 @@
 import styles from '../styles/components/_surface.module.css'
 
 type Props = {
-  children: React.ReactNode;
+  children: React.ReactNode,
+  props?: any
 }
 
 const Surface = ({ children, props }: Props) => {
